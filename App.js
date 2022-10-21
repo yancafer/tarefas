24e57d1413539cc7dc573fba3e7b08413e7dcf95
yancafer <yancafernandes2@gmail.com>
2022-10-21 10:47:24 -0500

    Adicionando a tela de cadastro de itens, criando interface tarefas

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, FlatList} from 'react-native';
 
 import Login from './src/components';
+import TaskList from './src/components/TaskList';
+
+let tasks = [
+  {key: '1', nome: 'Comprar coca cola'},
+  {key: '2', nome: 'Estudar javascript'}
+]
 
 export default function App(){
 
   const [user, setUser] = useState(null);
+
+  const [newTask, setNewTask] = useState('');
 
   if(!user){
     return <Login changeStatus={ (user) => setUser(user) }/>
@@ -14,7 +22,29 @@ export default function App(){
 
   return(
     <SafeAreaView style={styles.container}>
-      <Text>DENTRO DA TELA DE TAREFAS</Text>
+      
+      
+    <View style={styles.containerTask}>
+    <TextInput
+    style={styles.input}
+    placeholder= "O que vai fazer hoje?"
+    value={newTask}
+    onChangeText={(text) => setNewTask(text)}
+    />
+
+    <TouchableOpacity style={styles.buttonAdd}>
+      <Text style={styles.buttonText}>+</Text>
+    </TouchableOpacity>
+    </View>
+
+    <FlatList
+    data={tasks}
+    keyExtractor={ item => item.key}
+    renderItem={ ({item}) => (
+      <TaskList data={item}/>
+    )}
+    />
+
     </SafeAreaView>
   );
 }
@@ -25,5 +55,30 @@ const styles = StyleSheet.create({
     paddingTop: 25,
     paddingHorizontal: 10,
     backgroundColor: '#f2f6fc'
+  },
+  containerTask:{
+    flexDirection: 'row',
+  },
+  input:{
+    flex:1,
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#141414',
+    height: 45
+  },
+  buttonAdd:{
+    backgroundColor: '#141414',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 5,
+    paddingHorizontal: 14,
+    borderRadius: 4,
+  },
+  buttonText:{
+    color: '#fff',
+    fontSize: 22,
   }
 })
